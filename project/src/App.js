1b7a693ccd261ceb519749c46b02e5ff0efe5973
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-07-03 11:34:32 +0200

    start

---
+++ project/src/App.js
@@ -0,0 +1,21 @@
+//module
+import React, { Component } from 'react';
+//components
+
+//styles
+import './styles/base.scss';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <>
+          <h1>Hey</h1>
+        </>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
